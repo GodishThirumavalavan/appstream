36c52dfef505b6735e77b248d7f549cc1600f227
GodishThirumavalavan <71367239+GodishThirumavalavan@users.noreply.github.com>
2025-01-24 10:29:08 +0530

    Update build-image.sh
    Added VPCid, Subnet id, Doamin, Sg

--- build-image.sh
+++ build-image.sh
@@ -5,8 +5,12 @@ IMAGE_BUILDER_NAME="automated-appstream"
 INSTANCE_TYPE="stream.standard.large"
 BASE_IMAGE_NAME="AppStream-WinServer2016-06-17-2024"
 IAM_ROLE_ARN="arn:aws:iam::690797694046:role/appseteam-automation"
-#DIRECTORY_NAME="godingreen1.com"
-#OU_DISTINGUISHED_NAME="OU=AWS Reserved,DC=godingreen1,DC=com"
+DIRECTORY_NAME="goodin.com"
+OU_DISTINGUISHED_NAME="OU=AWS Reserved,DC=goodin.com,DC=com"
+VPC_ID="vpc-your-vpc-id"  # Add your VPC ID here
+SUBNET_ID="subnet-0fb5735fa5d14f1b3"  # Add your subnet ID here
+SECURITY_GROUP_ID="sg-025ddc4b02bb53af5"  # Add your security group ID here
+ENABLE_INTERNET_ACCESS=true  # Set to false if internet access isn't needed
 
 # Step 1: Create Image Builder
 aws appstream create-image-builder \
@@ -14,7 +18,11 @@ aws appstream create-image-builder \
   --instance-type "$INSTANCE_TYPE" \
   --image-name "$BASE_IMAGE_NAME" \
   --iam-role-arn "$IAM_ROLE_ARN" \
-  #--domain-join-info "{\"DirectoryName\":\"$DIRECTORY_NAME\",\"OrganizationalUnitDistinguishedName\":\"$OU_DISTINGUISHED_NAME\"}"
+  --domain-join-info "{\"DirectoryName\":\"$DIRECTORY_NAME\",\"OrganizationalUnitDistinguishedName\":\"$OU_DISTINGUISHED_NAME\"}"
+  --vpc-id "$VPC_ID" \
+  --subnet-id "$SUBNET_ID" 
+  --security-group-ids "$SECURITY_GROUP_ID" 
+  --enable-default-internet-access "$ENABLE_INTERNET_ACCESS"
 
 # Wait for the image builder to become available
 echo "Waiting for Image Builder to become available..."
